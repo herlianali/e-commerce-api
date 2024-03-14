736320e04966ee281cdb7a8898853e12da9d311e
herlianali <aliyasa58@gmail.com>
2024-03-14 15:31:25 +0700

    add socialite

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    //
+    public function redirect() 
+    {
+        return Socialite::driver('google')->redirect();
+    } 
+
+    public function callback()
+    {
+        $userFromGoogle = Socialite::driver('google')->user();
+
+        $userFromDatabase = Customer::where('id', $userFromGoogle->getId())->first();
+
+        if(!$userFromDatabase) {
+            $newCustomer = new Customer([
+                'id' => $userFromGoogle->getId(),
+                // 'name' => ,
+                // 'email' => ,
+            ]);
+        }
+    }
 }
